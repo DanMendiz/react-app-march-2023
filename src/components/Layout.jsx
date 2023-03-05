8a47fb1110f58b28077ad28d09dcec1047317236
DanMendiz <114294487+DanMendiz@users.noreply.github.com>
2023-03-05 18:32:18 +0000

    first UI draft

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import TempHeader from './TempHeader';
+import Container from '@mui/material/Container';
 
 function Layout() {
   return (
     <>
       <header>
         <h1>Cars App</h1>
-        <TempHeader/>
+        <TempHeader />
       </header>
       <main>
-        <Outlet />
+        <Container maxWidth="md">
+          <Outlet />
+        </Container>
       </main>
     </>
   );
--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,15 +1,68 @@
 import React from 'react';
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import FolderIcon from '@mui/icons-material/Folder';
+import EditIcon from '@mui/icons-material/Edit';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Typography from '@mui/material/Typography';
+import { ClassRounded } from '@mui/icons-material';
 
-function List() {
+function CarsList() {
+  const cars = [
+    {
+      _id: 1,
+      name: 'Ferrari',
+      bhp: 1234,
+      avatar_url:
+        'https://images.squarespace-cdn.com/content/v1/5d3a5c45ae39520001834147/1605196766876-NZERJ3O393E9L3ND8J7J/2020+Ferrari+488+Pista+Green-1.jpg',
+    },
+    {
+      _id: 2,
+      name: 'Bugatti',
+      bhp: 666,
+      avatar_url:
+        'https://cdn.motor1.com/images/mgl/WpAp1/s1/bugatti-chiron-pur-sport-in-lime-green.jpg',
+    },
+  ];
   return (
     <>
-      <div>List</div>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+      <Typography variant="h3" component="h2">
+        Cars
+      </Typography>
+
+      <List>
+        {cars.map(({ name, bhp, avatar_url, _id }) => (
+          <ListItem key={_id}>
+            <ListItemAvatar>
+              <Avatar alt="" src={avatar_url} />
+            </ListItemAvatar>
+            <ListItemText>
+              {name} (BHP: {bhp})
+            </ListItemText>
+            <IconButton
+              aria-label="update"
+              to={`/update/${_id}`}
+              component={Link}
+            >
+              <EditIcon />
+            </IconButton>
+            <IconButton
+              aria-label="delete"
+              onClick={() => console.log(`Delete ${_id}`)}
+            >
+              <DeleteIcon />
+            </IconButton>
+          </ListItem>
+        ))}
+      </List>
     </>
   );
 }
 
-export default List;
+export default CarsList;
