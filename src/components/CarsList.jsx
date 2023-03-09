8f37f88a63c164f0ac6891c7a0dae0f3ac32eed9
DanMendiz <114294487+DanMendiz@users.noreply.github.com>
2023-03-09 19:09:12 +0000

    reversed list

--- src/components/CarsList.jsx
+++ src/components/CarsList.jsx
@@ -15,9 +15,11 @@ function CarsList({
   cars = [],
   deleteHandler = () => console.log('No deleteHandler provided to Cars List'),
 }) {
+  const reversedCars = [...cars].reverse();
+  console.log(reversedCars);
   return (
     <List>
-      {cars.map(({ name, bhp, avatar_url, _id }) => (
+      {reversedCars.map(({ name, bhp, avatar_url, _id }) => (
         <ListItem key={_id}>
           <ListItemAvatar>
             <Avatar alt="" src={avatar_url} />
